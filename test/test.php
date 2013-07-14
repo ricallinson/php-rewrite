86eadb5fa0b585420b23dd8a30554d2a3e6d1618
Richard Allinson <capecodehq@yahoo.co.uk>
2013-07-14 12:46:59 -0700

    Travis setup

--- test/test.php
+++ test/test.php
@@ -27,5 +27,23 @@ describe("php-rewrite", function () {
             $result = \php_require\rewrite_routes\readRouteFromFile("foo");
             assert($result === null);
         });
+
+        it("should return GET /", function () {
+            $fixtures = __DIR__ . "/fixtures";
+            $result = \php_require\rewrite_routes\readRouteFromFile($fixtures . "/file-1.php");
+            assert($result === "GET /");
+        });
+
+        it("should return POST /", function () {
+            $fixtures = __DIR__ . "/fixtures";
+            $result = \php_require\rewrite_routes\readRouteFromFile($fixtures . "/file-2.php");
+            assert($result === "POST /");
+        });
+
+        it("should return GET /page.html", function () {
+            $fixtures = __DIR__ . "/fixtures";
+            $result = \php_require\rewrite_routes\readRouteFromFile($fixtures . "/file-3.php");
+            assert($result === "GET /page.html");
+        });
     });
 });
