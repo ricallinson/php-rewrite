53838bba360a3b02172e76fa59ba4be3439477f7
Ric Allinson <capecodehq@yahoo.co.uk>
2013-07-29 16:32:50 -0700

    Bug hide for rewrite base

--- index.php
+++ index.php
@@ -3,82 +3,82 @@ namespace php_require\rewrite_routes;
 
 function createApacheRewrite($route) {
 
-	$rule = "";
+    $rule = "";
 
-	// Get the method and path from the given route array.
-	list($method, $url) = explode(" ", trim($route["route"]), 2);
-	$filename = $route["filename"];
+    // Get the method and path from the given route array.
+    list($method, $url) = explode(" ", trim($route["route"]), 2);
+    $filename = $route["filename"];
 
-	// Remove the front slash
-	$regex = ltrim($url, "/");
+    // Remove the front slash
+    $regex = ltrim($url, "/");
 
-	// Swap * with (.*)
-	$regex = str_replace("*", "(.*)", $regex);
+    // Swap * with (.*)
+    $regex = str_replace("*", "(.*)", $regex);
 
-	// Wrap the regex so it has to be an exact match.
-	$regex = "^" . $regex . "$";
+    // Wrap the regex so it has to be an exact match.
+    $regex = "^" . $regex . "$";
 
-	// Make the rewrite rule.
-	$rule = "RewriteCond %{REQUEST_METHOD} " . strtoupper($method) . "\n";
-	$rule.= "RewriteCond %{REQUEST_FILENAME} !-f\n";
-	$rule.= "RewriteRule " . $regex . " " . $filename . " [L,QSA]\n";
+    // Make the rewrite rule.
+    $rule = "RewriteCond %{REQUEST_METHOD} " . strtoupper($method) . "\n";
+    $rule.= "RewriteCond %{REQUEST_FILENAME} !-f\n";
+    $rule.= "RewriteRule " . $regex . " " . $filename . " [L,QSA]\n";
 
-	// Return the rule.
-	return $rule;
+    // Return the rule.
+    return $rule;
 }
 
 function writeHtaccessFile($dir, $routes) {
 
-	$docroot = dirname($_SERVER["PHP_SELF"]);
-	$filename = $dir . DIRECTORY_SEPARATOR . ".htaccess";
-	$rules = array();
+    $docroot = "/";
+    $filename = $dir . DIRECTORY_SEPARATOR . ".htaccess";
+    $rules = array();
 
-	foreach ($routes as $route) {
-		array_push($rules, createApacheRewrite($route));
-	}
+    foreach ($routes as $route) {
+        array_push($rules, createApacheRewrite($route));
+    }
 
-	if (file_exists($filename)) {
-		unlink($filename);
-	}
+    if (file_exists($filename)) {
+        unlink($filename);
+    }
 
-	file_put_contents($filename, "RewriteEngine On\nRewriteBase " . $docroot . "\n\n" . implode("\n", $rules));
+    file_put_contents($filename, "RewriteEngine On\n#RewriteBase " . $docroot . "\n\n" . implode("\n", $rules));
 }
 
 function readRouteFromFile($filename) {
 
-	$marker = "@route";
+    $marker = "@route";
 
-	if (!file_exists($filename)) {
-		return null;
-	}
+    if (!file_exists($filename)) {
+        return null;
+    }
 
-	$content = file_get_contents($filename);
+    $content = file_get_contents($filename);
 
-	$start = strpos($content, $marker);
+    $start = strpos($content, $marker);
 
-	if ($start === false) {
-		return null;
-	}
+    if ($start === false) {
+        return null;
+    }
 
-	$start = $start + strlen($marker);
-	$end = strpos($content, "\n", $start);
+    $start = $start + strlen($marker);
+    $end = strpos($content, "\n", $start);
 
-	$routeString = trim(substr($content, $start, $end - $start));
+    $routeString = trim(substr($content, $start, $end - $start));
 
-	return $routeString;
+    return $routeString;
 }
 
 $module->exports = function ($dir, $type) {
 
-	$routes = array();
+    $routes = array();
 
-	if ($handle = opendir($dir)) {
+    if ($handle = opendir($dir)) {
         while (false !== ($entry = readdir($handle))) {
             if (strpos($entry, ".php") !== false) {
-            	$filename = $dir . DIRECTORY_SEPARATOR . $entry;
+                $filename = $dir . DIRECTORY_SEPARATOR . $entry;
                 $route = readRouteFromFile($filename);
                 if ($route) {
-                	array_push($routes, array("route" => $route, "filename" => $filename));
+                    array_push($routes, array("route" => $route, "filename" => $filename));
                 }
             }
         }
@@ -86,8 +86,8 @@ $module->exports = function ($dir, $type) {
     }
 
     if ($type === ".htaccess") {
-    	return writeHtaccessFile($dir, $routes);
+        return writeHtaccessFile($dir, $routes);
     } else if ($type === "httpd.conf") {
-    	return null;
+        return null;
     }
 };
